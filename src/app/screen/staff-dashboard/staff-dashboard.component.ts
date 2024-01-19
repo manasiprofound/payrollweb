25570cebfd8d1467c329eda9b622b35215a4924f
manasiprofound <manasi.j@itprofound.com>
2024-01-19 14:49:00 -0600

    almost done with dashboard and working in create staff

--- src/app/screen/staff-dashboard/staff-dashboard.component.ts
+++ src/app/screen/staff-dashboard/staff-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-staff-dashboard',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./staff-dashboard.component.scss']
 })
 export class StaffDashboardComponent {
+  constructor(private router: Router) { }
 
+  createemp() {
+    this.router.navigate(['createemp']);;
+  }
 }
